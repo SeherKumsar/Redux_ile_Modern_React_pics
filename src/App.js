6d131bd9e2021a65bd2a1adff98b7f67d69833d0
SeherKumsar <seherkumsar99@outlook.com>
2024-03-03 23:44:08 +0300

    added term from child component (SearchBar)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import SearchBar from "./components/SearchBar";
 
 function App () {
+
+    const handleSubmit = (term) => {
+        console.log('Do a search with', term);
+        // searchImages(term);
+    };
+
     return (
         <div>
             <SearchBar />
