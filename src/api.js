89fb20fee76c4c5af7347ef63fa0229b706eeaf1
SeherKumsar <seherkumsar99@outlook.com>
2024-03-03 23:00:29 +0300

    searched by replacing "cars" with "term" has been introduced

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const searchImages = async () => {
+const searchImages = async (term) => {
   const response = await axios.get("https://api.unsplash.com/search/photos", {
     headers: {
       Authorization: "Client-ID o7nKUSCzTmYPEOdTl96RpuWyop-pa36sNxoHSbiaPIE",
     },
     params: {
-      query: "cars",
+      query: term,
     },
   });
 
