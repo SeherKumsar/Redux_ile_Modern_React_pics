99bd3e84a7b46d14b470d61738cb8cb24b07f27e
SeherKumsar <seherkumsar99@outlook.com>
2024-03-04 02:30:32 +0300

    fetching a list of images from the API

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,22 +8,18 @@ function SearchBar({ onSubmit}) {
     const handleFormSubmit = (event) => {
         event.preventDefault(); // Prevent the page from refreshing when the form is submitted
         
-        onSubmit(
-            'asdasd'
-        );
+        onSubmit(term);
     };
 
     const handleChange = (event) => {
         // console.log(event.target.value); // used event.target.value instead of all event
-        setTerm(event.target.value.replace(/[a-z]/, ''));
+        setTerm(event.target.value);
     };
 
     return (
         <div>
             <form onSubmit={handleFormSubmit}>
-                Confirm your search: {term}
                 <input value={term} onChange={handleChange} />
-                {term.length < 3 && 'Term must be longer'}
             </form>
         </div>
     );
